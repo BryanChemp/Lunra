556e964c4ce7186ceaf51865b9026c57a6801a81
Bryan Chemp <bryan@inovaclick.com.br>
2025-09-25 17:44:26 -0300

    Fixxing brush proporcional size

--- src/features/DrawingCanvas/DrawingCanvas.tsx
+++ src/features/DrawingCanvas/DrawingCanvas.tsx
@@ -86,34 +86,37 @@ const DrawingCanvas: FC<Props> = ({ width, height, scale, offset, isPanning = fa
     const p = getLocalCanvasPoint(e);
     const last = lastPosRef.current;
     if (!last) {
-      lastPosRef.current = { x: p.x, y: p.y };
-      return;
+        lastPosRef.current = { x: p.x, y: p.y };
+        return;
     }
     const dx = p.x - last.x;
     const dy = p.y - last.y;
     const dist = Math.sqrt(dx * dx + dy * dy) || 0.0001;
+
     const spacingCanvas = (brush.spacing || 1) * p.cssToCanvasX;
-    const brushSizeCanvas = brush.size * p.cssToCanvasX;
+    const brushSizeCanvas = brush.size * scale * p.cssToCanvasX; // <---- aqui, multiplica pelo scale
+
     for (let i = 0; i < dist; i += spacingCanvas) {
-      const t = i / dist;
-      const ix = last.x + dx * t;
-      const iy = last.y + dy * t;
-      ctx.globalAlpha = brush.opacity || 1;
-      if (brush.shape instanceof HTMLImageElement) {
+        const t = i / dist;
+        const ix = last.x + dx * t;
+        const iy = last.y + dy * t;
+        ctx.globalAlpha = brush.opacity || 1;
+        if (brush.shape instanceof HTMLImageElement) {
         ctx.drawImage(brush.shape, ix - brushSizeCanvas / 2, iy - brushSizeCanvas / 2, brushSizeCanvas, brushSizeCanvas);
-      } else if (brush.shape === "square") {
+        } else if (brush.shape === "square") {
         ctx.fillStyle = brush.color;
         ctx.fillRect(ix - brushSizeCanvas / 2, iy - brushSizeCanvas / 2, brushSizeCanvas, brushSizeCanvas);
-      } else {
+        } else {
         ctx.fillStyle = brush.color;
         ctx.beginPath();
         ctx.arc(ix, iy, brushSizeCanvas / 2, 0, Math.PI * 2);
         ctx.fill();
-      }
-      ctx.globalAlpha = 1;
+        }
+        ctx.globalAlpha = 1;
     }
     lastPosRef.current = { x: p.x, y: p.y };
-  };
+    };
+
 
   return (
     <div>
